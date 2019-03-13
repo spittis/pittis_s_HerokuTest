9fbd41c4a5f0aeb34585a46b0d6381f1a9cabdf3
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-13 12:24:13 -0400

    fixed port

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.port || 3000; //set up a variable called port, which is inside the environment variable in port or make it 3000, the port will be 3000 when you run it
+const port = process.env.PORT || 3000; //set up a variable called port, which is inside the environment variable in port or make it 3000, the port will be 3000 when you run it
 
 app.use('/', indexRouter); //catching the root route here
 app.use('/users', usersRouter);
